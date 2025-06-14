b93c171f3039f9860ae8647c00cc403919b56b9c
eenrico77 <enrico.gnocato@gmail.com>
2025-06-14 18:01:33 +0200

    Delete src/auth.js

--- src/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// esempio di route
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from auth route!' });
-});
-
-module.exports = router;
